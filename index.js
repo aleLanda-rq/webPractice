99343a3948103ee31a009343b92350644092e805
Alejandra <aleriquene@gmail.com>
2022-11-16 15:06:49 -0600

    final commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+// fetch('https://randomuser.me/api/')
+// .then((response)=> response.json())
+// .then((data)=>displayInfo(data))
+
+// const firstimg=document.querySelector('.img-1');
+// const names= document.querySelector('.name')
+
+// function displayInfo(image, text){
+//     firstimg.src=image.picture;
+//     names.textContent=text.name;
+
+
+// }
